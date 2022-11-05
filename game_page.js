4d0f596062c0dae6c9ed7833e242e006c1bdaf9f
harinimukund <harinimukund@users.noreply.github.com>
2022-11-05 09:09:10 -0700

    Add files via upload

--- game_page.js
+++ game_page.js
@@ -14,9 +14,27 @@ function send(){
     actual_answer=parseInt("number1")*parseInt("number2");
     question_number="<h4>"+number1+"X"+number2+"</h4>";
     input_box='<br>Answer: <input type="text" id="input_check_box"></input>';
-    check_button='<br><br><button class="btn btn-info" onclick="check()"';
+    check_button='<br><br><button class="btn btn-info" onclick="check()">Check</button>';
     row=question_number+input_box+check_button;
     document.getElementById("output").innerHTML= row;
     document.getElementById("number1").value=""
     document.getElementById("number2").value=""
+}
+answer_turn="player2";
+question_turn="player1";
+function check(){
+    get_answer=document.getElementById("input_check_box").value;
+    if(answer_turn=="player1"){
+        update_player1_score=player1_score+1;
+        document.getElementById("player1_score").innerHTML=update_player1_score
+        answer_turn="player2"
+        document.getElementById("player_question").innerHTML="question_turn - " + player2_name
+    }
+    else{
+        update_player2_score=player2_score+1;
+        document.getElementById("player2_score").innerHTML=update_player2_score
+        answer_turn="player1"
+        document.getElementById("player_question").innerHTML="question_turn -" + player1_name
+
+    }
 }
